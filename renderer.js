6c91e9dba4ad788fc9a5d932a82426c9906497df
Bret Comnes <bcomnes@gmail.com>
2017-02-26 16:20:27 -0800

    first attemp didn't work so hot

--- renderer.js
+++ renderer.js
@@ -1,3 +1,19 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+console.log('hi!')
+console.log(process)
+
+var cool = require('cool-ascii-faces')
+
+console.log(cool())
+
+try {
+  var SerialPort = require('serialport')
+  console.log(SerialPort)
+} catch (e) {
+  console.error(e)
+  SerialPort = null
+}
+
